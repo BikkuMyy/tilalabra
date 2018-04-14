1d8bf3700ba2fe65d08510bb1addfe41b53d7549
BikkuMyy <maripartan@gmail.com>
2018-04-14 23:28:37 +0300

    javadocia lisätty, viikkoraportti päivitetty

--- src/main/java/datastructures/MyMap.java
+++ src/main/java/datastructures/MyMap.java
@@ -1,14 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package datastructures;
 
-/**
- *
- * @author mari
- */
-public class MyMap {
+import java.util.*;
+
+public class MyMap implements Map{
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Object get(Object key) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Object remove(Object key) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void putAll(Map m) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Set keySet() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Collection values() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Set entrySet() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+    /*
+    http://hg.openjdk.java.net/jdk8/jdk8/jdk/file/tip/src/share/classes/java/util/HashMap.java
+    https://www.geeksforgeeks.org/internal-working-of-hashmap-java/
+    */
     
 }
--- src/main/java/familytree/RelationFinder.java
+++ src/main/java/familytree/RelationFinder.java
@@ -3,6 +3,10 @@ package familytree;
 import java.util.*;
 import graph.*;
 
+/**
+ * Luokka sisältää toiminnallisuuden sukulaisuuksien määrittämiseen.
+ * @author mari
+ */
 public class RelationFinder {
 
     private Map<String, Boolean> visited;
@@ -12,10 +16,22 @@ public class RelationFinder {
         initializeMap(family);
     }
 
+    /**
+     * Metodi määrittää, ovatko parametreina annetut henkiöt sukua.
+     * @param start henkilö1
+     * @param end   henkilö2
+     * @return boolean
+     */
     public boolean areRelated(Person start, Person end) {
         return dfs(start, end);
     }
 
+    /**
+     * Metosi vastaa syvyyshaun toteutuksesta.
+     * @param start aloituskohta
+     * @param end   etsitty lopetus
+     * @return boolean
+     */
     public boolean dfs(Person start, Person end) {
         String endName = end.getName();
         ArrayDeque<Person> queue = new ArrayDeque();
@@ -36,7 +52,14 @@ public class RelationFinder {
         return false;
     }
 
-    public boolean dfsVisit(ArrayDeque<Person> queue, List<Person> processedList, String endName) {
+    /**
+     * Apumetodi syvyyshaulle.
+     * @param queue         jono
+     * @param processedList käsiteltävä lista
+     * @param endName       haettava henkilö
+     * @return boolean
+     */
+    private boolean dfsVisit(ArrayDeque<Person> queue, List<Person> processedList, String endName) {
         if (!processedList.isEmpty()) {
             for (Person next : processedList) {
                 String nextName = next.getName();
@@ -53,7 +76,11 @@ public class RelationFinder {
         return false;
     }
 
-    public void initializeMap(Family family) {
+    /**
+     * Apumetodi, joka populoi visited-listan.
+     * @param family 
+     */
+    private void initializeMap(Family family) {
         for (String name : family.getNames()) {
             this.visited.put(name, false);
         }
